1c4b5bb96519a09213b744b633e61537d9c1e30b
Keerti Bhatnagar <71969398+keertibhatnagar-it@users.noreply.github.com>
2021-09-29 15:27:39 +0530

    alert feature added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@
 import './Appa.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Alert } from './components/Alert';
 
 // import About from './components/About';
 function App() {
+  const [alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+  }
   const [mode, setMode] = useState('light');
   const toggleMode=()=>{
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor='#042743';
+      showAlert("Dark mode has been enabled", "success");
     }else{
       setMode('light');
       document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
   return (
    <>
 <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert}/>
 <TextForm heading="Enter Text here" mode={mode} toggleMode={toggleMode}/>
 {/* <About/> */}
    </>
