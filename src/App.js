9c1a86d40fe3e799ce9acf4a6cc4b3da1919c369
Keerti Bhatnagar <71969398+keertibhatnagar-it@users.noreply.github.com>
2021-09-29 12:28:19 +0530

    toggleMode feature added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@
 import './Appa.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+
 // import About from './components/About';
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+    }else{
+      setMode('light')
+    }
+  }
   return (
    <>
-<Navbar title="TextUtils" aboutText="About Us" />
+<Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
 <TextForm heading="Enter Text here"/>
 {/* <About/> */}
    </>
