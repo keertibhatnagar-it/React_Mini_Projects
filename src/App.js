9536c2b7f391b359584a9efe3380833477bd6992
Keerti Bhatnagar <71969398+keertibhatnagar-it@users.noreply.github.com>
2021-09-29 10:32:30 +0530

    About.js added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './Appa.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+// import About from './components/About';
 function App() {
   return (
    <>
 <Navbar title="TextUtils" aboutText="About Us" />
 <TextForm heading="Enter Text here"/>
+{/* <About/> */}
    </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,10 +7,35 @@ export default function TextForm(props) {
         let newText=text.toUpperCase();
         setText(newText);
     }
+    const handleClearClick=()=>{
+        let newText='';
+        setText(newText);
+    }
     const handleLowClick=(event)=>{
         let newText=text.toLowerCase();
         setText(newText);
     }
+    const handleInverseClick=()=>{
+        let newText='';
+        // for(let i=0;i<text.length;i++){
+        //     if (text[i]===text[i].toUpperCase()){
+        //        newText=text[i].toLowerCase();
+        //     }else if(text[i]===text[i].toLowerCase() ){
+        //         newText=text[i].toUpperCase();
+        //     }
+
+        
+        console.log(text.split(""));
+    }
+    const handleCopy=()=>{
+        let text=document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+const handleExtraSpaces=()=>{
+    let newText=text.split(/[ ]+/);
+    setText(newText.join(" "));
+}
     const handleOnChange=(event)=>{
         // console.log("handleOnChange clicked");
         setText(event.target.value);
@@ -23,10 +48,14 @@ export default function TextForm(props) {
         <div className="container my-4">
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                <textarea className="form-control" onChange={handleOnChange} value={text} id="exampleFormControlTextarea1" rows="8"></textarea>
+                <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
             
             <button className="btn btn-primary my-4 mx-2"  onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary my-4 mx-2"  onClick={handleLowClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary my-4 mx-2"  onClick={handleClearClick}>Clear</button>
+            <button className="btn btn-primary my-4 mx-2"  onClick={handleInverseClick}>Inverse Case</button>
+            <button className="btn btn-primary my-4 mx-2"  onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary my-4 mx-2"  onClick={handleExtraSpaces}>Cut Extra Spaces</button>
 
             </div> 
             </div>
